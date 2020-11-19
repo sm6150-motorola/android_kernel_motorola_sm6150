4dbe21cdfb4a05dbad3817ad0e88e0647ff15038
Danny Lin <danny@kdrag0n.dev>
2020-11-18 17:49:45 -0800

    power: smb5: Disable debug logging
    These debug logs are way too spammy.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -235,10 +235,7 @@ struct smb5 {
 	struct smb_dt_props	dt;
 };
 
-static int __debug_mask;
-module_param_named(
-	debug_mask, __debug_mask, int, 0600
-);
+static int __debug_mask = 0;
 
 static int __pd_disabled;
 module_param_named(
